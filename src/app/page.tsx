db3450cdd6351d907d5217e9a2c873ace2156cee
Jon Frisby <jfrisby@mrjoy.com>
2025-08-28 15:12:17 +0900

    UX improvements.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -81,44 +81,18 @@ export default function Home() {
   };
 
   return (
-    <div className="font-sans grid grid-rows-[40px_auto_200px] min-h-screen max-h-screen min-w-full p-2 pb-20 gap-4 sm:p-1">
-      <header className="row-start-1 w-full flex gap-4 justify-items-stretch">
-        <Button className="flex-4 basis-4 text-nowrap px-4" onClick={toggleLanguage}>
-          {sourceLanguage === "en-US" ? Us : Jp}
-          <span className="px-4">
-            →
-          </span>
-          {sourceLanguage === "en-US" ? Jp : Us}
-        </Button>
-
-        <Button
-          className="flex-8 basis-8 text-nowrap"
-          onClick={translate}
-          disabled={isTranslating || !sourceText.trim()}
-        >
-          {isTranslating ? "Translating..." : locale[sourceLanguage].translateButton}
-        </Button>
-
-        <span className="flex-4 basis-4" />
-
-        <Button className="flex-1" onClick={resetHistory}>
-          <span className="min-w-4 max-w-4 block">
-            X
-          </span>
-        </Button>
-      </header>
-
-      <main className="row-start-2 w-full flex flex-row items-center gap-x-4">
+    <div className="font-sans grid grid-rows-[auto_40px_200px] min-h-screen max-h-screen min-w-full p-2 pb-20 gap-4 sm:p-1">
+      <header className="row-start-1 w-full flex flex-row items-center gap-x-4">
         <textarea
           value={sourceText}
           onChange={(e) => setSourceText(e.target.value)}
           placeholder={placeholderMessage}
           rows={5}
-          className="flex-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none rounded-lg w-full"
+          className="flex-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none rounded-lg w-full h-full"
           lang={sourceLanguage}
           disabled={isTranslating}
         />
-        <div className="flex-1">
+        <div className="flex-1 h-full pt-1.5">
           {error ? (
             <div className="text-red-500 text-base">
               Error: {error}
@@ -137,6 +111,32 @@ export default function Home() {
             </div>
           )}
         </div>
+      </header>
+
+      <main className="row-start-2 w-full flex gap-4 justify-items-stretch">
+        <Button className="flex-4 basis-4 text-nowrap px-4" onClick={toggleLanguage}>
+          {sourceLanguage === "en-US" ? Us : Jp}
+          <span className="px-4">
+            →
+          </span>
+          {sourceLanguage === "en-US" ? Jp : Us}
+        </Button>
+
+        <Button
+          className="flex-8 basis-8 text-nowrap"
+          onClick={translate}
+          disabled={isTranslating || !sourceText.trim()}
+        >
+          {isTranslating ? "Translating..." : locale[sourceLanguage].translateButton}
+        </Button>
+
+        <span className="flex-4 basis-4" />
+
+        <Button className="flex-1 min-w-6 max-w-6" onClick={resetHistory}>
+          <span className="block">
+            X
+          </span>
+        </Button>
       </main>
 
       <footer className="row-start-3 w-full !max-h-[200px] overflow-scroll">
