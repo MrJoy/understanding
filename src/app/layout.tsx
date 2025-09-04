bf2a5f0a6e0ebabcbd9b6bde4f826002f8646cc9
Jon Frisby <jfrisby@mrjoy.com>
2025-09-04 19:13:55 +0900

    Strip this out.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,9 +17,6 @@ export default function RootLayout({
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <link rel="manifest" href="manifest.json" />
-        <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
         <meta name="format-detection" content="telephone=no" />
         <meta
           name="viewport"
